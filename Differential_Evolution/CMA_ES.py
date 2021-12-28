2be5b51660f225f28776196ed55a74e826496084
GergoGit <73502807+GergoGit@users.noreply.github.com>
2021-12-28 20:17:00 +0100

    add CMA_ES

---
+++ Differential_Evolution/CMA_ES.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 28 19:55:00 2021
+
+@author: bonnyaigergo
+"""
+
+# https://cma-es.github.io/
+# http://www.cmap.polytechnique.fr/~nikolaus.hansen/cmaes_inmatlab.html
+# https://github.com/CMA-ES/pycma/blob/master/cma/evolution_strategy.py
+# https://arxiv.org/pdf/1806.05845.pdf
