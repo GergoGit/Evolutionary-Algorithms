4ecf39436bb961a9937f1cecd2c11edd5ed4d86e
GergoGit <73502807+GergoGit@users.noreply.github.com>
2021-12-29 02:40:25 +0100

    Update BA_test.py

--- Differential_Evolution2/BA_test.py
+++ Differential_Evolution2/BA_test.py
@@ -5,3 +5,14 @@ Created on Thu Dec 23 23:23:56 2021
 @author: bonnyaigergo
 """
 
+import numpy as np
+from numba.experimental import jitclass
+
+bat_alg_spec = []
+
+@jitclass(bat_alg_spec)
+class BA(object):
+    def __init__(self, obj_func, population_size, iterations):
+        self.population_size = population_size
+        self.iterations = iterations
+        
