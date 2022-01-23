32d9fa4ac6220b792ac88990dea53904f7c0515b
GergoGit <73502807+GergoGit@users.noreply.github.com>
2022-01-23 03:43:48 +0100

    Unittest added 1st

--- Differential_Evolution2/StoppingCriterion.py
+++ Differential_Evolution2/StoppingCriterion.py
@@ -24,9 +24,12 @@ Distribution-based:
 function map
 metric(data)
 condition
+
+TODO: combine criteria
 """
 
 import numpy as np
+# import typing
 
 class EarlyStopping(object):
     """
@@ -38,14 +41,20 @@ class EarlyStopping(object):
         self.patience = 10
         self.tolerance = 1E-10
         self.n_best = 5
-        
 
-class ImpBest(object):
-    
+########################################
+# Improvement-based using Time Window
+########################################
+
+class ImpBestObj(object):
+    """
+    Checks whether the improvement of the best objective function value is below 
+    a threshold ('tolerance') for a number of generations ('patience'). 
+    'from_nth_gen' parameter means an initial number of
+    generations without checking the improvement.
+    """    
     def __init__(self):
-        
-        # super().__init__(from_nth_gen=0, patience=10, tolerance=1E-10, n_best=5)
-        
+                
         self.from_nth_gen = 0
         self.patience = 5
         self.tolerance = 1E-10
@@ -55,36 +64,44 @@ class ImpBest(object):
         self.check_list = np.empty(shape=(0,1))
             
         
-    def MeetStoppingCriterion(self, fitness, best_idx, nth_gen):
-        self.metric_list = np.append(self.metric_list, fitness[best_idx])
+    def MeetCriterion(self, fitness: float, best_idx: int, nth_gen: int) -> bool: 
         
-        if nth_gen > self.from_nth_gen:
+        if nth_gen >= self.from_nth_gen:
+            self.metric_list = np.append(self.metric_list, fitness[best_idx])
             check = np.abs(fitness[best_idx] - self.metric_list[-1]) < self.tolerance
             self.check_list = np.append(self.check_list, check)
-        if nth_gen >= (self.from_nth_gen + self.patience):
-            is_all_under_threshold = self.check_list[-self.patience:].all()            
-            return is_all_under_threshold
+            if nth_gen >= (self.from_nth_gen + self.patience):
+                is_all_under_threshold = self.check_list[-self.patience:].all()            
+                return is_all_under_threshold
+            else:
+                return False
         else:
             return False
         
 
         
-fitness=np.array([10])     
-es = EarlyStopping() 
-estop = ImpBestObj(from_nth_gen=0, patience=5, tolerance=1E-10, metric=fitness)
+# fitness=np.array([10])     
+# es = EarlyStopping() 
+# estop = ImpBestObj(from_nth_gen=0, patience=5, tolerance=1E-10, metric=fitness)
 
-estop = ImpBest()
-for i in range(10):
-    print(estop.MeetStoppingCriterion(nth_gen=i, metric=np.random.rand()))
+# estop = ImpBest()
+# for i in range(10):
+#     print(estop.MeetStoppingCriterion(nth_gen=i, metric=np.random.rand()))
 
-estop.metric_list
-estop.check_list
-estop.MeetStoppingCriterion(nth_gen=8, metric=fitness)
+# estop.metric_list
+# estop.check_list
+# estop.MeetStoppingCriterion(nth_gen=8, metric=fitness)
 
-estop.metric_list
-dir(estop)
+# estop.metric_list
+# dir(estop)
 
-class ImpAv(object):
+class ImpAvgObj(object):
+    """
+    Checks whether the improvement of the average objective function value is below 
+    a threshold ('tolerance') for a number of generations ('patience'). 
+    'from_nth_gen' parameter means an initial number of
+    generations without checking the improvement.
+    """  
     
     def __init__(self):
                 
@@ -97,27 +114,35 @@ class ImpAv(object):
         self.check_list = np.empty(shape=(0,1))
             
         
-    def MeetStoppingCriterion(self, fitness, nth_gen):
-        metric = np.mean(fitness)
-        self.metric_list = np.append(self.metric_list, metric)
+    def MeetCriterion(self, fitness: float, nth_gen: int) -> bool:
         
         if nth_gen > self.from_nth_gen:
+            metric = np.mean(fitness)
+            self.metric_list = np.append(self.metric_list, metric)
             check = np.abs(self.metric_list[-1] - self.metric_list[-2]) < self.tolerance
             self.check_list = np.append(self.check_list, check)
-        if nth_gen >= (self.from_nth_gen + self.patience):
-            is_all_under_threshold = self.check_list[-self.patience:].all()            
-            return is_all_under_threshold
+            if nth_gen >= (self.from_nth_gen + self.patience):
+                is_all_under_threshold = self.check_list[-self.patience:].all()            
+                return is_all_under_threshold
+            else:
+                return False
         else:
             return False
         
-estop = ImpAv()
-for i in range(10):
-    print(estop.MeetStoppingCriterion(nth_gen=i, fitness=np.random.rand()))       
+# estop = ImpAv()
+# for i in range(10):
+#     print(estop.MeetStoppingCriterion(nth_gen=i, fitness=np.random.rand()))       
     
-estop.metric_list
-estop.check_list
+# estop.metric_list
+# estop.check_list
 
-class MovPar(object):
+class ImpAvgPar(object):
+    """
+    Checks whether the improvement of the average objective function value is below 
+    a threshold ('tolerance') for a number of generations ('patience'). 
+    'from_nth_gen' parameter means an initial number of
+    generations without checking the improvement.
+    """ 
     
     def __init__(self):
         
@@ -132,20 +157,37 @@ class MovPar(object):
         self.check_list = np.empty(shape=(0,1))
             
         
-    def MeetStoppingCriterion(self, population, best_idx, nth_gen):
-        self.metric_list = np.append(self.metric_list, metric)
+    def MeetCriterion(self, population: float, best_idx: int, nth_gen: int) -> bool:
         
         if nth_gen > self.from_nth_gen:
-            check = (metric - self.metric_list[-1]) < self.tolerance
+            # Container
+            diff = np.empty(shape=(0, population.ndim))
+            
+            for idx in range(len(population)):
+                diff[idx] = np.linalg.norm(population[idx] - population[best_idx])
+                
+            metric = np.mean(diff)
+            self.metric_list = np.append(self.metric_list, metric)
+            check = np.abs(self.metric_list[-1] - self.metric_list[-2]) < self.tolerance
             self.check_list = np.append(self.check_list, check)
-        if nth_gen >= (self.from_nth_gen + self.patience):
-            is_all_under_threshold = self.check_list[-self.patience:].all()            
-            return is_all_under_threshold
+            if nth_gen >= (self.from_nth_gen + self.patience):
+                is_all_under_threshold = self.check_list[-self.patience:].all()            
+                return is_all_under_threshold
+            else:
+                return False
         else:
             return False
 
+##########################################################
+# Distribusion-based using metric related to 1 generation
+##########################################################
 
-class MaxDist(object):
+class MaxDistObj(object):    
+    """
+    Checks whether the max distance (objective space) of the individuals within the population 
+    is under a given threshold ('tolerance') from the individual of best fitness in a given generation.
+    'from_nth_gen' parameter means an initial number of generations without checking the improvement.
+    """
     
     def __init__(self):
                 
@@ -155,7 +197,7 @@ class MaxDist(object):
         # Container
         self.metric_list = np.empty(shape=(0,1))
                    
-    def MeetStoppingCriterion(self, fitness, nth_gen):
+    def MeetCriterion(self, fitness: float, nth_gen: int) -> bool:
         
         if nth_gen >= self.from_nth_gen:
             best_fitness = np.min(fitness)
@@ -166,16 +208,82 @@ class MaxDist(object):
         else:
             return False
         
-estop = MaxDist()
-for i in range(10):
-    print(estop.MeetStoppingCriterion(nth_gen=i, fitness=np.random.uniform(size=10)))       
+# estop = MaxDist()
+# for i in range(10):
+#     print(estop.MeetStoppingCriterion(nth_gen=i, fitness=np.random.uniform(size=10)))       
     
-estop.metric_list
+# estop.metric_list
 
-            
-class MaxDistPar(object):
+class MaxDistQuickObj(object):    
     """
-    max distance from the vector of best fitness
+    Checks whether the max distance (objective space) of the 'p_best' % best individuals 
+    within the population is under a given threshold ('tolerance') from the individual of best fitness 
+    in a given generation.
+    'from_nth_gen' parameter means an initial number of generations without checking the improvement.
+    """
+    
+    def __init__(self):
+                
+        self.from_nth_gen = 0
+        self.tolerance = 1E-10
+        self.p_best = 0.2
+
+        # Container
+        self.metric_list = np.empty(shape=(0,1))
+                   
+    def MeetCriterion(self, fitness: float, best_idx: int, nth_gen: int) -> bool:
+        
+        if nth_gen >= self.from_nth_gen:
+            k = np.round(len(fitness) * self.p_best)
+            k_best_idx = np.argsort(fitness)[:k]
+            # Container
+            diff = np.empty(shape=(0,1))
+            for idx in k_best_idx:
+                diff[idx] = fitness[idx] - fitness[best_idx]
+            metric = np.max(diff)
+            self.metric_list = np.append(self.metric_list, metric)
+            return metric < self.tolerance        
+        else:
+            return False
+
+
+class AvgDistObj(object):    
+    """
+    Checks whether the average distance (objective space) of the individuals within the population  
+    from the individual of best fitness in a given generation is under a given threshold ('tolerance') 
+    'from_nth_gen' parameter means an initial number of generations without checking the improvement.
+    """
+    
+    def __init__(self):
+                
+        self.from_nth_gen = 0
+        self.tolerance = 1E-10
+
+        # Container
+        self.metric_list = np.empty(shape=(0,1))
+                   
+    def MeetCriterion(self, fitness: float, best_idx: int, nth_gen: int) -> bool:
+                
+        if nth_gen >= self.from_nth_gen:
+            # Container
+            diff = np.empty(shape=(0,1))
+            
+            for idx in range(len(fitness)):
+                diff[idx] = fitness[idx] - fitness[best_idx]
+                
+            metric = np.mean(fitness)
+            self.metric_list = np.append(self.metric_list, metric)
+            return metric < self.tolerance        
+        else:
+            return False
+    
+    
+
+class AvgDistPar(object):
+    """
+    Checks whether the max (Euclidean) distance (objective space) of the individuals within the population 
+    is under a given threshold ('tolerance') from the individual of best fitness in a given generation.
+    'from_nth_gen' parameter means an initial number of generations without checking the improvement.
     """
     
     def __init__(self):
@@ -186,112 +294,51 @@ class MaxDistPar(object):
         # Container
         self.metric_list = np.empty(shape=(0,1))        
                    
-    def MeetStoppingCriterion(self, population, best_idx, nth_gen):
+    def MeetCriterion(self, population: float, best_idx: int, nth_gen: int) -> bool:
         
-        # Container
-        diff = np.empty(shape=(0, population.ndim))
-        
-        for idx in range(len(population)):
-            diff[idx] = np.linalg.norm(population[idx] - population[best_idx])
+        if nth_gen >= self.from_nth_gen:
+            # Container
+            diff = np.empty(shape=(0, population.ndim))
             
-        metric = np.max(diff)
-        self.metric_list = np.append(self.metric_list, metric)
-        return metric < self.tolerance 
+            for idx in range(len(population)):
+                diff[idx] = np.linalg.norm(population[idx] - population[best_idx])
+                
+            metric = np.mean(diff)
+            self.metric_list = np.append(self.metric_list, metric)
+            return metric < self.tolerance
+        else:
+            return False
     
-estop = MaxDistPar()
-for i in range(10):
-    print(estop.MeetStoppingCriterion(nth_gen=i, best_idx=1, population=np.random.uniform(size=(4,5))))       
+# estop = MaxDistPar()
+# for i in range(10):
+#     print(estop.MeetStoppingCriterion(nth_gen=i, best_idx=1, population=np.random.uniform(size=(4,5))))       
     
-estop.metric_list
+# estop.metric_list
 
 
 def StoppingCriterion(criterion):
         
-    criteria_func_map = {
-        'impbest': ImpBest,
-        'impav': ImpAv,
-        'movpar': MovPar,
-        'maxdist': MaxDist,
-        'maxdistpar': MaxDistPar
+    criteria_fn_map = {
+        'imp_best_obj': ImpBestObj,
+        'imp_avg_obj': ImpAvgObj,
+        'imp_avg_par': ImpAvgPar,
+        'max_dist_obj': MaxDistObj,
+        'max_distquick_obj': MaxDistQuickObj,
+        'avg_dist_obj': AvgDistObj,
+        'avg_dist_par': AvgDistPar
     }
     
-    assert criterion in criteria_func_map.keys(), 'Invalid criterion'
+    assert criterion in criteria_fn_map.keys(), 'Invalid criterion'
         
-    return criteria_func_map[criterion]
+    return criteria_fn_map[criterion]
 
 
-sc = StoppingCriterion(criterion='impbest')
+# sc = StoppingCriterion(criterion='impbest')
 
         
 if __name__ == '__main__':
-    m_list = np.asarray([1]*18)
-    stopping = SingleObjectiveEarlyStopping()
-    stopping.patience = 10
-    stopping.MeetStoppingCriterion(metric_list=m_list, nth_gen=12)
-    
-    
-    if nth_gen >= from_nth_gen + patience:
-        is_insig_imp = (np.asarray([np.linalg.norm(element-metric_list[-1]) for element in metric_list[-(from_nth_gen + patience):]]) < tolerance).all()            
-        return is_insig_imp
-    else:
-        return False
-    
-    class SingleObjectiveEarlyStopping(object):
-    """
-    
-    """
-    # # Container
-    # MetricList = np.empty(shape=(dim_num))
-    
-    # Store()
-    
-    def __init__(self):
-        self.from_nth_gen = 0
-        self.patience = 10
-        self.tolerance = 1E-10
-        self.n_best = 5
-    
-        
-    def Metric(data, n_best=None, aggr=None):
-        """
-        Parameters
-        ----------
-        data : TYPE NUMPY ARRAY
-            DESCRIPTION.
-        aggr : TYPE, optional
-            DESCRIPTION. The default is None. Can be np.mean(), np.std()
-
-        Returns
-        -------
-        TYPE NUMPY ARRAY
-            DESCRIPTION Chosen metric with or without aggregation
-        """
-        if aggr is not None:
-            metric = aggr(data, axis=0)
-            return metric
-        else:
-            return data
-        
-        
-    def Store(self, metric, metric_list):
-        metric_list = np.append(metric_list, metric)
-    
-    def MeetStoppingCriterion(self, metric_list, nth_gen):
-        """
-        Parameters
-        ----------
-        metric_list : TYPE NUMPY ARRAY
-            DESCRIPTION: list of metric 
-        nth_gen : TYPE INTEGER
-            DESCRIPTION: number of the actual generation (iteration) number
-            
-        Returns
-        -------
-        TYPE BOOLEAN
-            DESCRIPTION: Conditions meets the stopping criterion or not.
-        """
-        if nth_gen >= (self.from_nth_gen + self.patience):
-            is_insig_imp = (np.asarray([np.linalg.norm(element-metric_list[-1]) for element in metric_list[-(self.from_nth_gen + self.patience):]]) < self.tolerance).all()            
-            return is_insig_imp
-        else:
-            return False
+    # m_list = np.asarray([1]*18)
+    # stopping = SingleObjectiveEarlyStopping()
+    # stopping.patience = 10
+    # stopping.MeetStoppingCriterion(metric_list=m_list, nth_gen=12)
+    pass
